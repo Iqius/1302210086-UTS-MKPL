5c871b879a6fca620be88f816e40b9048c08182c
Iqius <iniiqius@gmail.com>
2024-04-21 14:36:05 +0700

    memisahkan method getAnnualIncomeTax menjadi 3 bagian

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -8,28 +8,26 @@ import java.util.List;
 public class Employee {
 
 	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+    
+    private LocalDate dateJoined;
+    private int monthWorkingInYear;
+    
+    private boolean isForeigner;
+    private boolean gender;
+    
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+    
+    private String spouseName;
+    private String spouseIdNumber;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
+    private List<String> childNames;
+    private List<String> childIdNumbers;
 	
 	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
@@ -37,9 +35,7 @@ public class Employee {
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+        this.dateJoined = dateJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
@@ -90,16 +86,24 @@ public class Employee {
 	}
 	
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
-	}
+        int monthWorkingInYear = calculateMonthsWorkedInYear();
+        boolean isSingle = isSingle();
+        int numberOfChildren = numberOfChildren();
+
+        return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, isSingle, numberOfChildren);
+    }
+
+    private int calculateMonthsWorkedInYear() {
+        LocalDate currentDate = LocalDate.now();
+        return (currentDate.getYear() == dateJoined.getYear()) ? currentDate.getMonthValue() - dateJoined.getMonthValue() : 12;
+    }
+
+    private boolean isSingle() {
+        return (spouseIdNumber == null || spouseIdNumber.isEmpty());
+    }
+
+    private int numberOfChildren() {
+        return childIdNumbers.size();
+    }
+
 }
